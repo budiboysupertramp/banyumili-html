ecaf1fec5b9f1963778a3c281b8b8bc8a6ee5520
budiboysupertramp <budiboy195@gmail.com>
2021-12-22 15:21:00 -0800

    gallery

---
+++ gallery.js
@@ -0,0 +1,17 @@
+document.addEventListener("click",function (e){
+    if(e.target.classList.contains("gallery-item")){
+        const src = e.target.getAttribute("src");
+        document.querySelector(".modal-img").src = src;
+        var myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+        myModal.show();
+    }
+})
+
+document.addEventListener("click",function (e){
+    if(e.target.classList.contains("gallery-item-2")){
+        const src = e.target.getAttribute("src");
+        document.querySelector(".modal-img-2").src = src;
+        var myModal = new bootstrap.Modal(document.getElementById('gallery-modal-2'));
+        myModal.show();
+    }
+})
